f21b2ace4429024ba711595cc19c4fab389abb1a
Anatoly Vasilyuk <67242808+tatarin-stan@users.noreply.github.com>
2024-06-24 17:51:00 +0300

    Add files via upload

---
+++ Js/1/Js.js
@@ -0,0 +1,88 @@
+// Робота зі змінними
+// Оголосіть дві змінні: name та city.
+// Присвойте значення "Іван" змінній name.
+// Скопіюйте значення зі змінної name в city.
+// Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
+
+
+// let name = "Ivan"
+// let city = name
+
+// console.log(city)
+
+
+//Який буде результат виконання скрипта?
+// let name = "Olga";
+// console.log(`привіт ${1}`); //
+// console.log(`привіт ${"name"}`); //
+// console.log(`привіт ${name}`); // ?
+
+//Видобути число зі змінних вивести в консоль тип
+
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+
+// console.log(typeof (Number.parseFloat(b)))
+
+
+//Зробіть, щоб 0.1 + 0.2 = 0.3
+
+// let a = 0.1;
+// let b =  0.2;
+// console.log(((a * 10) + (b * 10)) / 10);
+
+
+//Поверніть найбільше число с набору 20, 10, 50, 40
+// console.log(Math.max(10, 20, 30, 50 ,112))
+
+// console.log(Math.min(10, 20, 30, 50 ,112))
+
+
+//Поверніть випадкове число в діапазоні від 2 до 4
+
+// console.log(Math.random(2, 4))
+
+//дізнатись довжину message
+//  const message = "Welcome to Bahamas!";
+
+//  console.log(message.length)
+
+//вивести в консоль message великими літерами
+
+// console.log(message.toUpperCase())
+
+
+// створити пустий об*єкт
+// додати туди ім*я, вік і місто
+// вивести результат в консоль
+// видалити місто
+// додати буль з ключем: like flowers
+// вивести результат в консоль
+
+// let obj = {
+//     name: "Ivan",
+//     age: "30",
+//     city: "Kyiv"
+// }
+
+// console.log(obj)
+
+// delete obj.city;
+
+// obj["like flowers"] = true;
+
+// console.log(obj)
+
+//За допомогою циклу “for…in” вивести в консоль ключі і значення об*єкта
+
+
+
+// for(key in obj){
+//     console.log(obj[key])
+// }
+
+
+console.log("Rita")
+
+
